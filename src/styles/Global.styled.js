d187763f3ad2bc7a95177fae156d31ed0b788079
Yassir Irfan <yassirirfan7@gmail.com>
2023-08-15 22:07:31 +0530

    boilerplate

---
+++ src/styles/Global.styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const MainBody = styled.div`
+    background-color: ${(props) => props.theme.colors.primary};
+    height: 50px;
+    width: 100%; 
+`
